70997e5629f8271abbfb5942866ea141e438ca38
Damian (Daniel) Amalraj <damian.amalraj@hotmail.com>
2023-09-28 01:46:48 +0200

    Done with the initial assignment

---
+++ ChessBoard/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Unicode;
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+string[] list = new string[number];
+
+for (int i = 0; i < number; i++)
+{
+    if (i % 2 != 0)
+    {
+        list[0] = "◻︎";
+    }
+    else
+    {
+        list[0] = "◼︎";
+    }
+
+    for (int j = 0; j < number - 1; j++)
+    {
+        if (list[j] == "◻︎")
+        {
+            list[j + 1] = "◼︎";
+        }
+        else
+        {
+            list[j + 1] = "◻︎";
+        }
+    }
+    Console.WriteLine(string.Join(" ", list));
+    list = new string[number];
+}
